65ba4e16a2c67a0955d73fadfc1b26ac83f4cbfd
Brian Nganga <briannganga70@gmail.com>
2022-11-06 23:04:19 +0300

    Reusable ui

--- todo.py
+++ todo.py
@@ -1,29 +1,40 @@
 import flet
-from flet import Checkbox, Column, FloatingActionButton, Page, Row, TextField, icons
+from flet import Checkbox, Column, FloatingActionButton, Page, Row, TextField, UserControl, icons
+
+class TodoApp(UserControl):
+    def build(self):
+        self.new_task = TextField(hint_text="Whats needs to be done?", expand=True)
+        self.tasks = Column()
+
+        # application's root control (i.e. "view") containing all other controls
+        return Column(
+            width=600,
+            controls=[
+                Row(
+                    controls=[
+                        self.new_task,
+                        FloatingActionButton(icon=icons.ADD, on_click=self.add_clicked),
+                    ],
+                ),
+                self.tasks,
+            ],
+        )
+
+    def add_clicked(self, e):
+        self.tasks.controls.append(Checkbox(label=self.new_task.value))
+        self.new_task.value = ""
+        self.update()
 
 
 def main(page: Page):
-    def add_clicked(e):
-        tasks_view.controls.append(Checkbox(label=new_task.value))
-        new_task.value = ""
-        view.update()
-
-    new_task = TextField(hint_text="Whats needs to be done?", expand=True)
-    tasks_view = Column()
-    view = Column(
-        width=600,
-        controls=[
-            Row(
-                controls=[
-                    new_task,
-                    FloatingActionButton(icon=icons.ADD, on_click=add_clicked),
-                ],
-            ),
-            tasks_view,
-        ],
-    )
-
+    page.title = "ToDo App"
     page.horizontal_alignment = "center"
-    page.add(view)
+    page.update()
+
+    # create application instance
+    todo = TodoApp()
+
+    # add application's root control to the page
+    page.add(todo)
 
 flet.app(target=main)
