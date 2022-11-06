8223567d9ea266cd918f646933a275080af3f525
Brian Nganga <briannganga70@gmail.com>
2022-11-06 23:00:43 +0300

    row layout

--- todo.py
+++ todo.py
@@ -1,14 +1,29 @@
 import flet
-from flet import Checkbox, FloatingActionButton, Page, TextField, icons
+from flet import Checkbox, Column, FloatingActionButton, Page, Row, TextField, icons
+
 
 def main(page: Page):
     def add_clicked(e):
-        page.add(Checkbox(label=new_task.value))
+        tasks_view.controls.append(Checkbox(label=new_task.value))
         new_task.value = ""
-        page.update()
+        view.update()
 
-    new_task = TextField(hint_text="Whats needs to be done?")
+    new_task = TextField(hint_text="Whats needs to be done?", expand=True)
+    tasks_view = Column()
+    view = Column(
+        width=600,
+        controls=[
+            Row(
+                controls=[
+                    new_task,
+                    FloatingActionButton(icon=icons.ADD, on_click=add_clicked),
+                ],
+            ),
+            tasks_view,
+        ],
+    )
 
-    page.add(new_task, FloatingActionButton(icon=icons.ADD, on_click=add_clicked))
+    page.horizontal_alignment = "center"
+    page.add(view)
 
 flet.app(target=main)
